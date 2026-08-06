6be60e635abfc349c20bad762af588d101a433a7
Ephraim Mulilo <153711216+ephraimm-zm@users.noreply.github.com>
2026-08-06 23:20:03 +0200

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        result = []
+        lib = {")":"(", "}":"{","]":"["}
+        for char in s:
+            if char in lib:
+                if result and result[len(result) -1] == lib[char]:
+                    result.pop()
+                else:
+                    return False
+            else:
+                result.append(char)
+        return not result
